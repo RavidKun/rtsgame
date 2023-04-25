0e93da9b811a243cb9f2ecb0fbc0b96feb44663b
Ohad Cohen <ohadyy22@gmail.com>
2023-04-25 12:16:32 +0300

    Add conteol and zoom on map.

--- core/src/com/game7d/rtsgame/Scenes/DemoScreen.java
+++ core/src/com/game7d/rtsgame/Scenes/DemoScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -15,6 +16,7 @@ public class DemoScreen implements Screen {
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private Texture texture;
+    private float backdeltaX;
 
     public DemoScreen(rtsgame game) {
         this.game = game;
@@ -22,15 +24,29 @@ public class DemoScreen implements Screen {
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(400,240,gamecam);
         //gamecam.position.set(0,0,0);
+        backdeltaX=0;
 
     }
-    public void handleInput(float dt){
-        if(Gdx.input.isTouched())
-            gamecam.position.x+=100*dt;
+
+    public void handleInput(float dt) {
+        // Move the camera when two fingers are on the screen
+        if ( Gdx.input.isTouched(1)&& !Gdx.input.isTouched(2)) {
+            float deltaX = Gdx.input.getDeltaX(0) + Gdx.input.getDeltaX(1);
+            float deltaY = Gdx.input.getDeltaY(0) + Gdx.input.getDeltaY(1);
+            gamecam.translate(-deltaX * dt, deltaY * dt, 0);
 
 
-
+            float initialDistance = Vector2.dst(Gdx.input.getX(0), Gdx.input.getY(0), Gdx.input.getX(1), Gdx.input.getY(1));
+            float currentDistance = Vector2.dst(Gdx.input.getX(0) + Gdx.input.getDeltaX(0), Gdx.input.getY(0) + Gdx.input.getDeltaY(0), Gdx.input.getX(1) + Gdx.input.getDeltaX(1), Gdx.input.getY(1) + Gdx.input.getDeltaY(1));
+            float distanceDiff = currentDistance - initialDistance;
+            if (distanceDiff > 50) {
+                gamecam.zoom -= 0.04f;
+            } else if (distanceDiff < -50) {
+                gamecam.zoom += 0.04f;
+            }
+        }
     }
+
     private void update(float delta) {
         handleInput(delta);
         gamecam.update();
